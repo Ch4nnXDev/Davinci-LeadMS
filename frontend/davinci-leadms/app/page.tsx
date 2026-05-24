d2769b08045cfd2c0f4b31763d626aa36c17736e
Channa2146@gmail.com <Channa2146@gmail.com>
2026-05-24 18:09:55 +0530

    pafe fix

--- frontend/davinci-leadms/app/page.tsx
+++ frontend/davinci-leadms/app/page.tsx
@@ -1,14 +1,26 @@
-import Image from "next/image";
+'use client'
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  function getStartedButton() {
+    router.push("/main");
+  }
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans h-full p-5">
-      <h1 className="text-4xl font-bold text-gray-900 dark:text-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-green-50 h-screen">
+      <h1 className="text-4xl font-bold justify-center items-center">
         Welcome to Davinci LeadMS
       </h1>
-      <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
+      <p className="mt-4 text-lg">
         Your all-in-one lead management system.
       </p>
+      <Button onClick={getStartedButton} className="m-10 p-5 bg-white hover:bg-blue-400 rounded-xl">
+        Get Started
+      </Button>
+      
     </div>
   );
 }
