3efb7daaca195313ab6b889a944d4c3ab7965cb6
Channa2146@gmail.com <Channa2146@gmail.com>
2026-05-26 09:27:53 +0530

    wrongly commited

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 axios.get('https://graph.facebook.com/v25.0/1105445742659281', {
     headers: {
-        Authorization: "Bearer EAAQUkMRB5cMBRmmhRBhuAI3yj9G12VZBywcJ16uPhxf7DzXT3lTuTtylpAmQypvEZB4uJjv4WWK7nX3E23fPFjAkjowzU4M5ttIQfNpb5OCESklwZAWEkrFjO7ZAwfAZBh2lOfz8h9P46lwLpZCAxHNgv0cz5pw7ZCxuU4d3EqEwbF7O2xmLYwK7BN09GPg4urY29QyDX0G3exVmgNk1mXr7BgWDvkNacnquzHtY37sJX7TZBQZBpmD283Okn6qjIt2sDjZBxnPlZB6iZAEW1ARh700j",
+        Authorization: "Bearer YOUR_ACCESS_TOKEN",
     },
 }).then((response) => {
     console.log(response.data);
