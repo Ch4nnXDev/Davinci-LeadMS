cb946e3ee9f47b6c612cfd3f2648827f86b1fc48
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-20 17:02:01 +0530

    transitioning to node backend into SpringBoot so all node files are removed

--- services/whatsappService.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://graph.facebook.com/v25.0/1105445742659281', {
-    headers: {
-        Authorization: "Bearer YOUR_ACCESS_TOKEN",
-    },
-}).then((response) => {
-    console.log(response.data);
-})
